8cb88e54eb0656e57471f1234894f5973ab0d930
Alex-Nuzdans <145292987+Alex-Nuzdans@users.noreply.github.com>
2026-03-25 09:28:23 +0500

    Update analysis.py
    Сообщение

--- analysis.py
+++ analysis.py
@@ -2,7 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def confusion_matrix(y_true: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
-
+'''
+Эта же функция с теми же переменными существует в
+sklearn.metrics, так что если есть желание подключите её.
+'''
     n_classes = max(np.unique(y_pred))+1
     matrix = np.zeros((n_classes, n_classes),dtype=int)
     for i,j in zip(y_true, y_pred):
